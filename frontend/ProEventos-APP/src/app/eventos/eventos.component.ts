adaaa4bb1948244d2f712f42fc6c58b0d750506f
Diego <diego_sousa.s@outlook.com>
2022-02-03 20:28:57 -0300

    Criando projeto angular e inserindo fontawesome e bootstrap

---
+++ frontend/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  eventos: any;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  private getEventos(): void{
+    this.httpClient.get("https://localhost:5001/api/eventos").subscribe(
+      response => this.eventos = response,
+      error => console.error(error)
+    );
+  }
+
+}
